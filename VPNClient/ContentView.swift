692584b097f688a807d99d8c0965b856af1f2bd3
kean <grebenyuk.alexander@gmail.com>
2020-05-09 10:58:25 -0400

    Initial commit

---
+++ VPNClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VPNClient
+//
+//  Created by Alexander Grebenyuk on 09.05.2020.
+//  Copyright © 2020 kean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
